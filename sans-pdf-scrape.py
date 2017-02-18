2110753b816bb9e521072facce1f33d88cbd3bf5
bitMuch <bearwolffish@gmail.com>
2017-02-18 00:53:51 +0000

    [+] Comments

--- sans-pdf-scrape.py
+++ sans-pdf-scrape.py
@@ -3,21 +3,21 @@
 # On: 17/02/2017
 # Task: Retrieve all Sans whitepapers
 # --- Preamble --- #
-from bs4 import BeautifulSoup			# funtions for parsing html
-from urllib.request import urlopen		# functions for d/l web page
-from urllib.request import urlretrieve
+from bs4 import BeautifulSoup			                              # funtions for parsing html
+from urllib.request import urlopen		                          # function for opening web page
+from urllib.request import urlretrieve                          # function for saving pdfs to disk
 # --- Definitions --- #
 ingredients = 'https://uk.sans.org/reading-room/whitepapers/auditing/index.html'
-root = ingredients[:19]				# strip root url for use later 
+root = ingredients[:19]				                                  # strip root url for use later 
 # --- Functions --- #
 def main():
-  soup = BeautifulSoup(urlopen(ingredients), 'html.parser')
-  for link in soup.find_all('a'):
-    link = (link.get('href'))
-    if '/reading-room/whitepapers/auditing/' in link:
-      pdl = ('%s%s' % (root, link))
-      pdf = ('%s.pdf' % (pdl[54:]))
-      urlretrieve(pdl, pdf)
+  soup = BeautifulSoup(urlopen(ingredients), 'html.parser')     # open page
+  for link in soup.find_all('a'):                               # find links
+    link = (link.get('href'))                                   # find string in link
+    if '/reading-room/whitepapers/auditing/' in link:           # we only want pdfs
+      pdl = ('%s%s' % (root, link))                             # build full link to pdf
+      pdf = ('%s.pdf' % (pdl[54:]))                             # save file as
+      urlretrieve(pdl, pdf)                                     # retreive link, save ffile
 # --- Main --- #
 if __name__ == "__main__":
   main()
